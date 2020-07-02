5193e6909a16c57e2fbbae10762a8d56129c0506
hwangbo <taekwonod714@naver.com>
2020-07-03 01:41:06 +0900

    feat: Context를 이용한 TODO 앱 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import {TodosContextProvider} from "./contexts/TodosContext";
 
 function App() {
   return (
-      <div>
+      <TodosContextProvider>
         <TodoForm/>
         <TodoList/>
-      </div>
+      </TodosContextProvider>
   );
 }
 
